f21341c82a91a998342e1089f83c2202ba3dda0e
tomislav98 <tomislavdz98@gmail.com>
2025-05-20 14:45:37 +0200

    Extract and store API key from Config class.
    The API key is now a top-level constant,
    avoiding the need for redundant imports
    of the Config class.
    This makes it easier to use and manage
    in other parts of the app.

--- lib/config.dart
+++ lib/config.dart
@@ -3,3 +3,5 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 class Config {
   static String get apiKey => dotenv.env['API_KEY'] ?? '';
 }
+
+final String WEATHER_API_KEY = Config.apiKey;
--- lib/services/weather_api.dart
+++ lib/services/weather_api.dart
@@ -1,12 +1,12 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import '../models/weather.dart';
-import 'package:weather_app/config.dart';
+import 'package:weather_app/config.dart' show WEATHER_API_KEY;
 
 Future<Weather> fetchWeatherData(String city) async {
   final response = await http.get(
     Uri.parse(
-      'https://api.weatherapi.com/v1/forecast.json?key=${Config.apiKey}&q=$city&days=3',
+      'https://api.weatherapi.com/v1/forecast.json?key=${WEATHER_API_KEY}&q=$city&days=3',
     ),
   );
 
